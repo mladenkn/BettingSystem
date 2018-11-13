69d7ba0e123b42cfc874073f492c04d489b8d335
mladen <mladen.knezovic.1993@gmail.com>
2018-11-13 14:54:12 +0100

    implemented Database class

---
+++ ApplicationKernel/Database.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApplicationKernel
+{
+    public interface IDatabase
+    {
+        Task SaveChanges();
+    }
+
+    public class Database : IDatabase
+    {
+        private readonly DbContext _dbContext;
+
+        public Database(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task SaveChanges() => _dbContext.SaveChangesAsync();
+    }
+}

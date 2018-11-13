e590236fb3a8342edc2b487a953aa366743a7962
mladen <mladen.knezovic.1993@gmail.com>
2018-11-13 14:58:12 +0100

    added TicketController

---
+++ BettingSystem.RestApi/Controllers/TicketController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using BetingSystem.Requests;
+using BetingSystem.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BetingSystem.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketController : ControllerBase
+    {
+        private readonly ITicketService _ticketService;
+
+        public TicketController(ITicketService ticketService)
+        {
+            _ticketService = ticketService;
+        }
+
+        [HttpPost]
+        public async Task Post([FromBody] CommitTicketRequest request)
+        {
+            var userId = ""; // TODO
+            await _ticketService.Handle(request, userId);
+        }
+    }
+}
